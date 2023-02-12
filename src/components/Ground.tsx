d7063a6d8da60d202d103626c06246751ffd2b8e
Mario Ferrero <47794126+mariofdezzz@users.noreply.github.com>
2023-02-12 16:59:21 +0000

    feat: add ground texture

--- src/components/Ground.tsx
+++ src/components/Ground.tsx
@@ -1,15 +1,22 @@
 import { usePlane } from '@react-three/cannon'
+import { Textures } from '../utils/textures'
 
 export function Ground (): JSX.Element {
+  const SIZE = 100
+  const DIMENSIONS = [SIZE, SIZE] as const
+
   const [ref] = usePlane(() => ({
     rotation: [-Math.PI / 2, 0, 0],
     position: [0, -0.5, 0]
   }))
 
+  const texture = Textures.grass.top.clone()
+  texture.repeat.set(...DIMENSIONS)
+
   return (
     <mesh ref={ref}>
       <planeBufferGeometry attach='geometry' args={[100, 100]} />
-      <meshStandardMaterial attach='material' color='lightgreen' />
+      <meshStandardMaterial attach='material' map={texture} color='#B0E876' />
     </mesh>
   )
 }
